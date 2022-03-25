93b577345a6feb5a430574e618c27f0529298a30
Daniel Hall <dhalla@miumg.edu.gt>
2022-03-25 10:37:07 -0600

    PremioG5

--- Lab1_G5_Entrega_Final/proyecto_juego/src/gamemap.cpp
+++ Lab1_G5_Entrega_Final/proyecto_juego/src/gamemap.cpp
@@ -156,7 +156,7 @@ void gamemap::drawVictoria()
 {
     string line;
     char userInput = ' ';
-    ifstream myFile("premio.txt");
+    ifstream myFile("premioG5.txt");
     if(myFile.is_open())
     {
         //Se obtiene el mapa externo y se general el mapa de celdas
