8f506707611a35dbfff6ff1eac7288ae05dafdeb
abigail vasquez <abivasquez576@gmail.com>
2022-03-25 11:01:39 -0600

    modificacion de mensaje final

--- Lab1_G5_Entrega_Final/proyecto_juego/src/player.cpp
+++ Lab1_G5_Entrega_Final/proyecto_juego/src/player.cpp
@@ -64,7 +64,7 @@ void player::callInput()
         //cout << "El jugador se mueve izquierda" << endl;
         break;
     case 'x':
-        cout << " -- Juego Terminado -- :(" << endl << endl;
+        cout << " -- el juego ha llegado a su fin, gracias por jugar soldado . G5-- :D" << endl << endl;
         drawCreditos();
         cout << endl;
         exit(0);
