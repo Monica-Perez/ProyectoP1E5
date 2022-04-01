585b99f3c306c5e091dd8cd9cc4e0db7c9f5426d
Monica-Perez <mperezv32@miumg.edu.gt>
2022-04-01 17:49:04 -0600

    SubiendoProyectos

---
+++ SistemaRH/src/Empleados.cpp
@@ -0,0 +1,19 @@
+#include "Empleados.h"
+#include "Empleados.h"
+#include <iostream>
+
+using namespace std;
+
+Empleados::Empleados()
+{
+    //ctor
+    cout<<"\t\t\t\t------------------"<<endl;
+	cout<<"\t\t\t\t |   EMPLEADOS  |"<<endl;
+	cout<<"\t\t\t\t------------------"<<endl;
+
+}
+
+Empleados::~Empleados()
+{
+    //dtor
+}
